a4397027009582226bd67334b50354efab444cc8
xiang <aa0955059329@gmail.com>
2025-02-03 15:32:53 +0800

    base4

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import App from './App.jsx'
 import "./assets/all.scss"
 import { BrowserRouter } from 'react-router-dom'
 
+const basename = process.env.NODE_ENV === 'production' ? '/react-week2' : '/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
